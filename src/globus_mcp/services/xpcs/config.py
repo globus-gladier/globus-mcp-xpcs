fb1451e2f616260ac5dde9d8bf7460f7c64ac67f
Nickolaus D. Saint <nick@globus.org>
2026-07-07 12:53:31 -0700

    Added config module

--- src/globus_mcp/services/xpcs/config.py
+++
@@ -1,9 +0,0 @@
-GLOBUS_ENDPOINT_VOYAGER = "ed03fe07-632e-47a9-9df7-9b8b236ac0b4"
-GLOBUS_ENDPOINT_EAGLE_APS_DATA_PROCESSING = "98d26f35-e5d5-4edd-becf-a75520656c64"
-GLOBUS_COMPUTE_ENDPOINT = "f8f4692a-0ab7-40d0-b256-ba5b82b5e2ec"
-GLOBUS_COMPUTE_ENDPOINT_USER_CONFIG = {
-    "queue": "debug",
-    "walltime": "1:00:00",
-    "nodes_per_block": 1,
-    "max_blocks": 5,
-}
