febb7d947cc181ca5ba6da83b1f70ba666477d0a
Nickolaus D. Saint <nick@globus.org>
2026-07-21 10:34:26 -0700

    Update config with new endpoint, clarify filesystems for collections

--- src/globus_mcp/config.py
+++ src/globus_mcp/config.py
@@ -3,15 +3,13 @@ from __future__ import annotations
 from collections.abc import Iterable
 from typing import Any
 
-# GLOBUS_ENDPOINT_VOYAGER = "ed03fe07-632e-47a9-9df7-9b8b236ac0b4"
-# GLOBUS_ENDPOINT_EAGLE_APS_DATA_PROCESSING = "98d26f35-e5d5-4edd-becf-a75520656c64"
-# GLOBUS_COMPUTE_ENDPOINT = "f8f4692a-0ab7-40d0-b256-ba5b82b5e2ec"
-DEFAULT_COMPUTE_ENDPOINT = "f8f4692a-0ab7-40d0-b256-ba5b82b5e2ec"
+DEFAULT_COMPUTE_ENDPOINT = "d88919ea-026a-493e-9124-fe3c46defa54"
 COMPUTE_ENDPOINTS: list[dict[str, Any]] = [
     {
         "uuid": "d88919ea-026a-493e-9124-fe3c46defa54",
         "display_name": "Globus Compute Endpoint",
-        "description": "Globus Compute Endpoint for XPCS processing",
+        "description": "Globus Compute Endpoint for XPCS processing on eagle",
+        "filesystem": "eagle",
         "allowed_basepaths": [
             {
                 "path": "/eagle/APSDataProcessing/aps8idi/xpcs_staging/agentic-testing/",
@@ -32,11 +30,16 @@ COLLECTIONS: list[dict[str, Any]] = [
         "type": "collection",
         "uuid": "ed03fe07-632e-47a9-9df7-9b8b236ac0b4",
         "display_name": "Voyager",
-        "description": "Globus Collection for source data for XPCS processing",
+        "filesystem": "voyager",
+        "description": "The voyager Globus Collection containing source datasets for xpcs",
         "collection_basepath": "/",
         "allowed_basepaths": [
             {
                 "path": "/8IDI/2025-2/tempus202507-merge/data/converted",
+                "permissions": "r",
+            },
+            {
+                "path": "/8IDI/2025-2/tempus202507-merge/data/",
                 "permissions": "r",
             }
         ],
@@ -45,6 +48,7 @@ COLLECTIONS: list[dict[str, Any]] = [
         "type": "collection",
         "uuid": "98d26f35-e5d5-4edd-becf-a75520656c64",
         "display_name": "Eagle APS Data Processing",
+        "filesystem": "eagle",
         "description": "Globus Collection for source data for XPCS processing",
         "collection_basepath": "/eagle/APSDataProcessing/aps8idi/",
         "allowed_basepaths": [
